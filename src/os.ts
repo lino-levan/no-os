a0c5481967d1863bfa08d2e2db0a5e8ad40db7c1
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-02-23 10:32:55 -0800

    chore: support vite build

--- src/os.ts
+++ src/os.ts
@@ -1,54 +1,56 @@
 import * as syscall from "./syscalls.ts";
 import { boot } from "./boot.ts";
 
-await boot();
+(async () => {
+  await boot();
 
-const context: {
-  env: Record<string, string>;
-} = {
-  env: {
-    PATH: "/bin",
-    PWD: "/home/default",
-    USER: "root",
-  },
-};
-
-self.onmessage = async (event) => {
-  const command: string[] = event.data.split(" ");
-
-  // replace $ENV
-  for (let i = 0; i < command.length; i++) {
-    if (command[i].startsWith("$")) {
-      command[i] = context.env[command[i].slice(1)];
-    }
-  }
-
-  let stdout = "";
-
-  const real_log = console.log;
-
-  console.log = (...text) => {
-    stdout += text.join(" ");
+  const context: {
+    env: Record<string, string>;
+  } = {
+    env: {
+      PATH: "/bin",
+      PWD: "/home/default",
+      USER: "root",
+    },
   };
 
-  try {
-    const text = await syscall.readText("/bin/" + command[0] + ".js");
-    const { default: run } = await import(
-      /* @vite-ignore */ "data:text/javascript," + text
-    );
+  self.onmessage = async (event) => {
+    const command: string[] = event.data.split(" ");
 
-    await run({
-      cmd: command,
-      env: context.env,
-      syscall,
-    });
+    // replace $ENV
+    for (let i = 0; i < command.length; i++) {
+      if (command[i].startsWith("$")) {
+        command[i] = context.env[command[i].slice(1)];
+      }
+    }
 
-    await self.postMessage({ command: "result", stdout });
-  } catch (err) {
-    await self.postMessage({ command: "result", stdout: stdout + err });
-  }
+    let stdout = "";
 
-  console.log = real_log;
-};
+    const real_log = console.log;
 
-self.postMessage({ command: "ready" });
+    console.log = (...text) => {
+      stdout += text.join(" ");
+    };
+
+    try {
+      const text = await syscall.readText("/bin/" + command[0] + ".js");
+      const { default: run } = await import(
+        /* @vite-ignore */ "data:text/javascript," + text
+      );
+
+      await run({
+        cmd: command,
+        env: context.env,
+        syscall,
+      });
+
+      await self.postMessage({ command: "result", stdout });
+    } catch (err) {
+      await self.postMessage({ command: "result", stdout: stdout + err });
+    }
+
+    console.log = real_log;
+  };
+
+  self.postMessage({ command: "ready" });
+})();
